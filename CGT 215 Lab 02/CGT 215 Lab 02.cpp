c2f97b486b45abd0f0b18ab385d0d5fa8f0db8c9
Yichen Dai <dai229@purdue.edu>
2022-08-30 14:15:23 -0400

    CGT 215 Lab 2

--- CGT 215 Lab 02/CGT 215 Lab 02.cpp
+++ CGT 215 Lab 02/CGT 215 Lab 02.cpp
@@ -8,14 +8,15 @@ int main()
 	float B;
 	float X;
 
+	cout << "Hello, my name is Yichen and I' m going to solve the equation:" << endl;
+	cout << "AX + B = 0 , For X" << endl;
 	cout << "Please enter a value for A: ";
 	cin >> A;
 
 	cout << "Please enter a value for B: ";
 	cin >> B;
 
-	cout << "A=" << A << endl;
-	cout << "B=" << B << endl;
+	cout << "A=" << A << " , " << "B=" << B << endl;
 
 	X = -B/ A;
 	
